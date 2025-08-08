6827b3cc6875624992093b75fc19eee51a88ec66
tobiajras <tobyajras@gmail.com>
2025-08-08 15:39:23 -0300

    test 3

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -13,30 +13,44 @@ export default function PublicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div
-      className='min-h-screen w-full bg-fixed bg-cover bg-center bg-no-repeat'
-      style={{
-        backgroundImage: 'url(/assets/inicio/background.jpg)',
-      }}
-    >
-      {company.whatsapp && (
-        <a
-          href={`https://api.whatsapp.com/send?phone=549${company.whatsapp[0]}&text=Hola! Quería hacer una consulta`}
-          target='_blank'
-          rel='noopener noreferrer'
-          className='fixed bottom-0 right-0 mx-5 my-10 md:m-10 lg:m-16 xl:m-20 z-50 hover:scale-110 transition-all duration-300 hover:drop-shadow-xl'
-        >
-          <Image
-            priority
-            className='drop-shadow-xl w-16 h-16 md:w-20 md:h-20'
-            src='/assets/whatsapp-banner/whatsapp-fixed.svg'
-            width={80}
-            height={80}
-            alt='whatsapp logo'
-          />
-        </a>
-      )}
-      {children}
+    <div className='relative min-h-screen w-full'>
+      {/* Fondo fijo para mobile */}
+      <div
+        className='fixed inset-0 w-full h-screen bg-cover bg-center bg-no-repeat md:hidden'
+        style={{
+          backgroundImage: 'url(/assets/inicio/background.jpg)',
+        }}
+      />
+
+      {/* Fondo para desktop */}
+      <div
+        className='hidden md:block fixed inset-0 w-full h-screen bg-cover bg-center bg-no-repeat'
+        style={{
+          backgroundImage: 'url(/assets/inicio/background.jpg)',
+        }}
+      />
+
+      {/* Contenido */}
+      <div className='relative z-10'>
+        {company.whatsapp && (
+          <a
+            href={`https://api.whatsapp.com/send?phone=549${company.whatsapp[0]}&text=Hola! Quería hacer una consulta`}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='fixed bottom-0 right-0 mx-5 my-10 md:m-10 lg:m-16 xl:m-20 z-50 hover:scale-110 transition-all duration-300 hover:drop-shadow-xl'
+          >
+            <Image
+              priority
+              className='drop-shadow-xl w-16 h-16 md:w-20 md:h-20'
+              src='/assets/whatsapp-banner/whatsapp-fixed.svg'
+              width={80}
+              height={80}
+              alt='whatsapp logo'
+            />
+          </a>
+        )}
+        {children}
+      </div>
     </div>
   );
 }
